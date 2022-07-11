a4c43f6c519d1d7dbf4cc7c15f9130685160c9b3
Rodrigo Nunes <nunesrodrigo13@outlook.com>
2022-07-11 11:44:10 -0300

    test(setup): test configuration

---
+++ jest.config.js
@@ -0,0 +1,37 @@
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const customJestConfig = {
+  coverageDirectory: 'coverage',
+  moduleDirectories: ["node_modules", "<rootDir>/"],
+  testEnvironment: 'jest-environment-jsdom',
+  testPathIgnorePatterns: [
+    '<rootDir>/.next/',
+    '<rootDir>/node_modules/',
+  ],
+  setupFiles: [
+    '<rootDir>/__mocks__/common.js'
+  ],
+  moduleNameMapper: {
+    "util/(.*)$": "<rootDir>/src/util/$1",
+    "theme(.*)$": "<rootDir>/src/theme/$1",
+    "components/(.*)$": "<rootDir>/src/components/$1",
+  }
+}
+
+module.exports = createJestConfig(customJestConfig)
+
+// const asyncConfig = createJestConfig(customJestConfig)
+
+// module.exports = async () => {
+//   const config = await asyncConfig()
+
+//   // next/jest ignores node_modules and allows to add more ignore patterns, but we need to override them fully to whitelist some node_modules
+//   // https://github.com/vercel/next.js/blob/canary/packages/next/build/jest/jest.ts
+//   config.transformIgnorePatterns = customJestConfig.transformIgnorePatterns
+
+//   return config
+// }
